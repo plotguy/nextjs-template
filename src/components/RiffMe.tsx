6fc4312e6d3e65edd34339bc0058d2442c5f871f
Hayk Martiros <hayk@corpusant.ai>
2023-03-23 22:17:29 +0000

    Customize with a simple component

---
+++ src/components/RiffMe.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface RiffMeProps {}
+
+const RiffMe: React.FC<RiffMeProps> = ({}) => {
+  return (
+    <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+      ✨🎸 Let's Riff! 🎸✨
+    </h1>
+  );
+};
+
+export default RiffMe;
